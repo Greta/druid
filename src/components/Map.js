e55c130bec825f89ef0d7660e06d9de05cc6ceb8
Greta <gretagail@gmx.com>
2019-12-23 16:16:25 -0500

    Creating initial test map

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,15 +3,28 @@ import { connect } from 'react-redux';
 
 class Map extends Component {
   render() {
-    console.log(this.props);
-    return <div>Map</div>;
-  }
-}
+    return (
+      <table>
+        <tbody>
+        {this.props.map.map((row, rowIndex) => {
+          return (
+            <tr key={rowIndex}>
+              {row.map((square, squareIndex) => {
+                return <td key={squareIndex} className={square}></td>
+              })}
+            </tr>
+          );
+        })}
+        </tbody>
+      </table>
+    );
+  };
+};
 
 const mapStateToProps = state => {
   return {
     map: state.map
   };
-}
+};
 
 export default connect(mapStateToProps)(Map);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,21 @@
 import { combineReducers } from 'redux';
 
 const mapReducer = () => {
-  return [
-    ['','','',''],
-    ['','','',''],
-    ['','','',''],
-    ['','','','']
-  ];
+  const mapWidth = 8,
+        mapHeight = 8,
+        terrains = ['G', 'P', 'W', 'S', 'F'];
+
+  // Build default test map
+  let map = [], i;
+  for (i = 0; i < mapHeight; i++) {
+    let mapRow = [], j;
+    for (j = 0; j < mapWidth; j++) {
+      mapRow.push(terrains[Math.floor(Math.random() * terrains.length)])
+    }
+    map.push(mapRow);
+  }
+
+  return map;
 };
 
 const playerLocationReducer = (playerLocation = [1, 1], action) => {
